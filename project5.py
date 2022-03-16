51c35acb6c883b7f1c48c12bbc0b0f11eef5d9d0
Apoorv Tiwari <93195855+ApoorvTiwar1109@users.noreply.github.com>
2022-03-16 19:03:30 +0530

    Add files via upload

---
+++ project5.py
@@ -0,0 +1,4 @@
+print("Enter the word")
+d = {"fascinating":"extremely interesting", "marvelous":"extraordinary", "gigantic":"huge or enormous", "programming":"the act of writing computer programs", "introvert":"a reticent person"}
+x = input()
+print("The meaning of", x, "is", d[x], end =".")
